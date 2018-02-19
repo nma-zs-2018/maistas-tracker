20c08cad6d50f8ba88b7c4b1ca0ae0e23a4443ae
Joris Pevcas <joris@pevcas.eu>
2018-02-19 21:54:04 +0200

    Creating the groups and improving the models

--- maistas/models.py
+++ maistas/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Food(models.Model):
     def __str__(self):
         return self.food_name
@@ -8,12 +9,13 @@ class Food(models.Model):
     food_name = models.CharField(max_length=100)
 
 
-
 class Order(models.Model):
     deliver_by = models.ForeignKey(User, default=None, related_name='deliver_by', on_delete=models.DO_NOTHING)
     deliver_to = models.ForeignKey(User, default=None, related_name='deliver_to', on_delete=models.DO_NOTHING)
-    deliver_to_lat = models.FloatField()
-    deliver_to_long = models.FloatField()
-    current_delivery_lat = models.FloatField()
-    current_delivery_long = models.FloatField()
+    deliver_to_lat = models.FloatField(default=0)
+    deliver_to_long = models.FloatField(default=0)
+    current_delivery_lat = models.FloatField(default=0)
+    current_delivery_long = models.FloatField(default=0)
     food = models.ForeignKey(Food, default=None, on_delete=models.DO_NOTHING)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    main = models.BooleanField(default=False)
